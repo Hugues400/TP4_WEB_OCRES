4a9b939afefb8ef028a4c221d0472617f40d2f8d
Hugues Puiseux <hugues.puiseux@edu.ece.fr>
2021-10-15 19:41:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Text from './text.js';
+import Bouton from './bouton.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Bouton name="Jeanne" />
+    <Bouton name="Martine" />
+    <Bouton name="Claude" />
     </div>
   );
 }
---
+++ src/bouton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Bouton extends React.Component{
+    render(){
+        return(
+            <bouton>{this.props.name}</bouton>
+
+        );
+    }
+}
