c60cabec8ca04aea70964afa60c6eb4222e44e8b
Hugues Puiseux <hugues.puiseux@edu.ece.fr>
2021-11-10 14:06:02 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-
-export default class Bouton extends React.Component{
-    render(){
-        return(
-            <button>{this.props.name}</button>
-
-        );
-    }
-}
+import Bouton from './Bouton';
 
 function App() {
   return (
     <div>
-    <Bouton name="Jeanne" />
-    <Bouton name="Martine" />
-    <Bouton name="Claude" />
+      <Bouton name="Jeanne"/>
+      <Bouton name="Martine"/>
+      <Bouton name="Claude"/>
     </div>
   );
 
 
 }
 
-//export default App;
+export default App;
