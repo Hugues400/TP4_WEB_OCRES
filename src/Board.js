3e1472bc02734618600d8f9e3d5e0dfb9f56e23b
Hugues Puiseux <hugues.puiseux@edu.ece.fr>
2021-11-19 10:01:44 +0000

    ajout board

--- src/Board.js
+++ src/Board.js
@@ -5,9 +5,14 @@ export default class Board extends React.Component{
     render(){
         return(
             <div>
-                
-            </div>
+                <div>
 
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            
         );
     }
 }
